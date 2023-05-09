8cad7051ee7621a97be59eb703203da043210f9c
Johan <johan.svartdal30@gmail.com>
2023-05-09 16:19:14 +0200

    Updated takedown

--- takedown.sh
+++ takedown.sh
@@ -2,5 +2,9 @@
 kubectl delete deployment wordpress -n web
 kubectl delete deployment nginx -n web
 kubectl delete deployment mariadb -n web
+kubectl delete service wordpress -n web
+kubectl delete service nginx -n web
+kubectl delete service mariadb -n web
+kubectl delete configmap mariadb-initdb-config -n web
 kubectl delete pvc wp-pvc -n web
 kubectl delete pv wp-pv
